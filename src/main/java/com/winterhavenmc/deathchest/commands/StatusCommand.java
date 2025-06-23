ecd2789875716eb35b88e702f314ce063e6400e8
Tim Savage <tim.savage@gmail.com>
2025-06-22 21:02:08 -0500

    convert all lines of status display to configurable messages in language file using MessageBuilderLib for display

--- src/main/java/com/winterhavenmc/deathchest/commands/StatusCommand.java
+++ src/main/java/com/winterhavenmc/deathchest/commands/StatusCommand.java
@@ -18,14 +18,20 @@
 package com.winterhavenmc.deathchest.commands;
 
 import com.winterhavenmc.deathchest.PluginMain;
+import com.winterhavenmc.deathchest.messages.Macro;
 import com.winterhavenmc.deathchest.messages.MessageId;
 import com.winterhavenmc.deathchest.permissions.protectionplugins.ProtectionPlugin;
 import com.winterhavenmc.deathchest.sounds.SoundId;
+import com.winterhavenmc.library.messagebuilder.pipeline.formatters.duration.BoundedDuration;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
+import java.time.Duration;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
+
+import static com.winterhavenmc.deathchest.messages.Macro.*;
 
 
 /**
@@ -55,50 +61,164 @@ final class StatusCommand extends SubcommandAbstract
 			return true;
 		}
 
-		String versionString = this.plugin.getDescription().getVersion();
-		sender.sendMessage(ChatColor.DARK_AQUA + "DeathChest " + ChatColor.AQUA + "Version: "
-				+ ChatColor.RESET + versionString);
+		displayStatusBanner(sender);
+		displayDebugSetting(sender);
+		displayPluginVersion(sender);
+		displayLanguage(sender);
+		displayLocale(sender);
+		displayTimezone(sender);
+		displayExpirationTime(sender);
+		displayProtectionTime(sender);
+		displaySearchDistance(sender);
+		displayRequireChest(sender);
+		displayQuickLoot(sender);
+		displayKillerLooting(sender);
+		displayProtectionPlugins(sender);
+		displayEnabledWorlds(sender);
+		displayReplaceableBlocks(sender);
+		displayStatusFooter(sender);
 
+		return true;
+	}
+
+
+	private void displayStatusBanner(final CommandSender sender)
+	{
+		plugin.messageBuilder.compose(sender, MessageId.COMMAND_STATUS_BANNER)
+				.setMacro(Macro.PLUGIN, plugin.getDescription().getName())
+				.send();
+	}
+
+
+	private void displayStatusFooter(final CommandSender sender)
+	{
+		plugin.messageBuilder.compose(sender, MessageId.COMMAND_STATUS_FOOTER)
+				.setMacro(Macro.PLUGIN, plugin.getDescription().getName())
+				.setMacro(Macro.URL, "https://github.com/winterhavenmc/MessageBuilderLib")
+				.send();
+	}
+
+
+	private void displayPluginVersion(final CommandSender sender)
+	{
+		plugin.messageBuilder.compose(sender, MessageId.COMMAND_STATUS_PLUGIN_VERSION)
+				.setMacro(Macro.VERSION, plugin.getDescription().getVersion())
+				.send();
+	}
+
+
+	private void displayDebugSetting(final CommandSender sender)
+	{
 		if (plugin.getConfig().getBoolean("debug"))
 		{
 			sender.sendMessage(ChatColor.DARK_RED + "DEBUG: true");
 		}
+	}
 
-		sender.sendMessage(ChatColor.GREEN + "Language: "
-				+ ChatColor.RESET + plugin.getConfig().getString("language"));
 
-		int expireTime = plugin.getConfig().getInt("expire-time");
-		if (expireTime == 0)
-		{
-			expireTime = -1;
-		}
+	private void displayLanguage(final CommandSender sender)
+	{
+		plugin.messageBuilder.compose(sender, MessageId.COMMAND_STATUS_LANGUAGE)
+				.setMacro(Macro.LANGUAGE, plugin.getConfig().getString("language"))
+				.send();
+	}
 
-		sender.sendMessage(ChatColor.GREEN + "Chest Expiration: "
-				+ ChatColor.RESET + plugin.messageBuilder.getTimeString(TimeUnit.MINUTES.toMillis(expireTime)));
 
-		int chestProtectionTime = plugin.getConfig().getInt("chest-protection-time");
-		if (chestProtectionTime == 0)
-		{
-			chestProtectionTime = -1;
-		}
+	private void displayLocale(final CommandSender sender)
+	{
+		plugin.messageBuilder.compose(sender, MessageId.COMMAND_STATUS_LOCALE)
+				.setMacro(Macro.LOCALE, plugin.getConfig().getString("locale"))
+				.send();
+	}
 
-		sender.sendMessage(ChatColor.GREEN + "Chest Protection: "
-				+ ChatColor.RESET + plugin.messageBuilder
-				.getTimeString(TimeUnit.MINUTES.toMillis(chestProtectionTime)));
 
-		sender.sendMessage(ChatColor.GREEN + "Search Distance: "
-				+ ChatColor.RESET + plugin.getConfig().getString("search-distance"));
+	private void displayTimezone(final CommandSender sender)
+	{
+		plugin.messageBuilder.compose(sender, MessageId.COMMAND_STATUS_TIMEZONE)
+				.setMacro(TIMEZONE, plugin.getConfig().getString("timezone", ZoneId.systemDefault().toString()))
+				.send();
+	}
 
-		sender.sendMessage(ChatColor.GREEN + "Require Chest: "
-				+ ChatColor.RESET + plugin.getConfig().getString("require-chest"));
 
-		sender.sendMessage(ChatColor.GREEN + "Quick Loot: "
-				+ ChatColor.RESET + plugin.getConfig().getString("quick-loot"));
+	private void displayProtectionTime(final CommandSender sender)
+	{
+		// if config setting is zero, use negative number to represent unlimited time
+		int protectionTime = plugin.getConfig().getInt("protection-time");
+		if (protectionTime == 0) { protectionTime = -1; }
 
-		sender.sendMessage(ChatColor.GREEN + "Killer Looting: "
-				+ ChatColor.RESET + plugin.getConfig().getString("killer-looting"));
+		BoundedDuration boundedDuration = new BoundedDuration(Duration.ofMinutes(protectionTime), ChronoUnit.MINUTES);
 
-		sender.sendMessage(ChatColor.GREEN + "Protection Plugin Support:");
+		plugin.messageBuilder.compose(sender, MessageId.COMMAND_STATUS_PROTECTION_TIME)
+				.setMacro(PROTECTION_DURATION, boundedDuration)
+				.send();
+	}
+
+
+	private void displayExpirationTime(final CommandSender sender)
+	{
+		// if config setting is zero, use negative number to represent unlimited time
+		int expirationTime = plugin.getConfig().getInt("expire-time");
+		if (expirationTime == 0) { expirationTime = -1; }
+
+		BoundedDuration boundedDuration = new BoundedDuration(Duration.ofMinutes(expirationTime), ChronoUnit.MINUTES);
+
+		plugin.messageBuilder.compose(sender, MessageId.COMMAND_STATUS_EXPIRATION_TIME)
+				.setMacro(EXPIRATION_DURATION, boundedDuration)
+				.send();
+	}
+
+
+	private void displaySearchDistance(final CommandSender sender)
+	{
+		plugin.messageBuilder.compose(sender, MessageId.COMMAND_STATUS_SEARCH_DISTANCE)
+				.setMacro(SEARCH_DISTANCE, plugin.getConfig().getString("search-distance"))
+				.send();
+	}
+
+
+	private void displayRequireChest(final CommandSender sender)
+	{
+		plugin.messageBuilder.compose(sender, MessageId.COMMAND_STATUS_REQUIRE_CHEST)
+				.setMacro(REQUIRE_CHEST, plugin.getConfig().getString("require-chest"))
+				.send();
+	}
+
+
+	private void displayQuickLoot(final CommandSender sender)
+	{
+		plugin.messageBuilder.compose(sender, MessageId.COMMAND_STATUS_QUICK_LOOT)
+				.setMacro(Macro.QUICK_LOOT, plugin.getConfig().getString("quick-loot"))
+				.send();
+	}
+
+
+	private void displayKillerLooting(final CommandSender sender)
+	{
+		plugin.messageBuilder.compose(sender, MessageId.COMMAND_STATUS_KILLER_LOOTING)
+				.setMacro(Macro.KILLER_LOOTING, plugin.getConfig().getString("killer-looting"))
+				.send();
+	}
+
+
+	private void displayEnabledWorlds(final CommandSender sender)
+	{
+		plugin.messageBuilder.compose(sender, MessageId.COMMAND_STATUS_ENABLED_WORLDS)
+				.setMacro(Macro.ENABLED_WORLDS, plugin.worldManager.getEnabledWorldNames().toString())
+				.send();
+	}
+
+
+	private void displayReplaceableBlocks(final CommandSender sender)
+	{
+		plugin.messageBuilder.compose(sender, MessageId.COMMAND_STATUS_REPLACEABLE_BLOCKS)
+				.setMacro(Macro.REPLACEABLE_BLOCKS, plugin.chestManager.getReplaceableBlocks())
+				.send();
+	}
+
+
+	private void displayProtectionPlugins(final CommandSender sender)
+	{
+		plugin.messageBuilder.compose(sender, MessageId.COMMAND_STATUS_PROTECTION_PLUGINS).send();
 
 		int count = 0;
 		for (ProtectionPlugin protectionPlugin : plugin.protectionPluginRegistry.getAll())
@@ -132,14 +252,6 @@ final class StatusCommand extends SubcommandAbstract
 		{
 			sender.sendMessage(ChatColor.AQUA + "  [ NONE ENABLED ]");
 		}
-
-		sender.sendMessage(ChatColor.GREEN + "Enabled Worlds: " + ChatColor.RESET +
-				plugin.worldManager.getEnabledWorldNames().toString());
-
-		sender.sendMessage(ChatColor.GREEN + "Replaceable Blocks: " + ChatColor.RESET +
-				plugin.chestManager.getReplaceableBlocks());
-
-		return true;
 	}
 
 }
