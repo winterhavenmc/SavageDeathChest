a0e5d5e4e5e85bf39c17cbd5175c145a28457d5c
Tim Savage <tim.savage@gmail.com>
2025-06-24 12:48:04 -0500

    add back towny repository/dependency

---
+++ src/main/java/com/winterhavenmc/deathchest/permissions/protectionplugins/plugins/Towny.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2022-2025 Tim Savage.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.winterhavenmc.deathchest.permissions.protectionplugins.plugins;
+
+import com.palmergames.bukkit.towny.object.TownyPermission;
+import com.palmergames.bukkit.towny.utils.PlayerCacheUtil;
+import com.winterhavenmc.deathchest.permissions.protectionplugins.ProtectionPlugin;
+import com.winterhavenmc.deathchest.permissions.protectionplugins.ProtectionPluginAbstract;
+import me.angeschossen.lands.api.integration.LandsIntegration;
+import me.angeschossen.lands.api.land.Area;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
+
+
+/**
+ * Class that implements protection plugin checks for Towny
+ */
+public final class Towny extends ProtectionPluginAbstract implements ProtectionPlugin
+{
+	/**
+	 * Class constructor
+	 *
+	 * @param plugin reference to SavageDeathChest plugin main class instance
+	 * @param name name of the protection plugin
+	 * @param version version of the protection plugin
+	 */
+	public Towny(final JavaPlugin plugin, final String name, final String version)
+	{
+		this.plugin = plugin;
+		this.name = name;
+		this.version = version;
+	}
+
+
+	@Override
+	public boolean allowChestPlacement(final Player player, final Location location)
+	{
+		// use try...catch block to gracefully handle exceptions thrown by protection plugin
+		try
+		{
+			return PlayerCacheUtil.getCachePermission(player, location,
+					Material.CHEST, TownyPermission.ActionType.BUILD);
+		}
+		catch (Error | Exception e)
+		{
+			logPlaceError(e.getLocalizedMessage());
+			if (plugin.getConfig().getBoolean("debug")) {
+				e.printStackTrace();
+			}
+		}
+		// if all else fails, allow placement
+		return true;
+	}
+
+
+	@Override
+	public boolean allowChestAccess(final Player player, final Location location)
+	{
+		try
+		{
+			LandsIntegration landsIntegration = new LandsIntegration(plugin);
+			final Area area = landsIntegration.getAreaByLoc(location);
+			return Objects.requireNonNull(area).hasFlag(player, me.angeschossen.lands.api.flags.Flags.INTERACT_CONTAINER, false);
+		}
+		catch (Error | Exception e)
+		{
+			logAccessError(e.getLocalizedMessage());
+			if (plugin.getConfig().getBoolean("debug"))
+			{
+				e.printStackTrace();
+			}
+		}
+		// if all else fails, allow access
+		return true;
+	}
+
+}
