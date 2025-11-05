9cad6611647cf15d5db5ac6cfe52a414be455b5c
Tim Savage <tim.savage@gmail.com>
2025-11-05 01:13:45 -0600

    reorder checks; early exit if no server ops exist

--- src/main/java/com/winterhavenmc/deathchest/chests/search/AbstractSearch.java
+++ src/main/java/com/winterhavenmc/deathchest/chests/search/AbstractSearch.java
@@ -157,20 +157,14 @@ abstract class AbstractSearch implements Search
 	 */
 	private boolean isSpawnProtected(final Location location)
 	{
-		// check for null parameter
-		if (location == null)
-		{
-			return false;
-		}
-
-		// if location world is null, return false
-		if (location.getWorld() == null)
-		{
-			return false;
-		}
-
 		// if no server ops, spawn protection is disabled
 		if (plugin.getServer().getOperators().isEmpty())
+		{
+			return false;
+		}
+
+		// check for null parameter, null location world
+		if (location == null || location.getWorld() == null)
 		{
 			return false;
 		}
